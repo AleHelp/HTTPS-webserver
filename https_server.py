21bad90f1ab851737bd317a99bf2b180370523b8
Alessandro Eleuteri <126585573+AleHelp@users.noreply.github.com>
2023-12-29 14:22:18 +0100

    Update https_server.py
    Fixing and adding the no-https mode

--- https_server.py
+++ https_server.py
@@ -1,39 +1,64 @@
 #!/usr/bin/env python3
-import http.server
+
+from http.server import SimpleHTTPRequestHandler, HTTPServer
 import socketserver
 import ssl
 import argparse
 import os
+import socket
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Simple web server over HTTPS.')
-    parser.add_argument('-i', '--ip', type=str, default="0.0.0.0", help='Specify an IP address (default: 127.0.0.1)')
+    parser.add_argument('-i', '--ip', type=str, default="0.0.0.0", help='Specify an IP address (default: 0.0.0.0)')
     parser.add_argument('-p', '--port', type=int, default=443, help='Select a port (default: 443)')
     parser.add_argument('-d', '--directory', type=str, default="./", help='Select a directory (default is the current directory where you start it)')
     parser.add_argument('-c', '--cert', type=str, help='Specify a certificate to use')
     parser.add_argument('-k', '--key', type=str, help='Specify a private key')
+    parser.add_argument('--no-https', action='store_true', help='Serve over HTTP.')
     parser.epilog = ('To generate a self-signed certificate and the corresponding private key, use the following command: openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365.')
     return parser.parse_args()
 
-args = parse_arguments()
-directory_to_serve = args.directory
+def serve_http(args):
+    print(f"[+] Serving directory at http://{args.ip}:{args.port} from {args.directory}")
+    os.chdir(args.directory)
+    server_class = HTTPServer
+    server_address = (args.ip, 80)
+    with server_class(server_address, SimpleHTTPRequestHandler) as httpd:
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("\n[!] Server stopped by user.")
 
-if args.cert is None or not os.path.isfile(args.cert):
-    print(f"[*] Invalid or missing certificate.")
-    exit(1)
+def serve_https(args):
+    if not os.path.isfile(args.cert) or not os.path.isfile(args.key):
+        print("[-] Invalid or missing certificate or key.")
+        exit(1)
 
-if args.key is None or not os.path.isfile(args.key):
-    print(f"[*] Invalid or missing key.")
-    exit(1)
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    ssl_context.load_cert_chain(certfile=args.cert, keyfile=args.key)
 
-class MyHandler(http.server.SimpleHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, directory=directory_to_serve, **kwargs)
+    print(f"[+] Serving directory at https://{args.ip}:{args.port} from {args.directory}")
+    httpd = socketserver.TCPServer((args.ip, args.port), SimpleHTTPRequestHandler)
+    httpd.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-ssl_context.load_cert_chain(certfile=args.cert, keyfile=args.key)
+    with httpd:
+        httpd.socket = ssl_context.wrap_socket(httpd.socket, server_side=True)
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("\n[!] Server stopped by user.")
 
-with socketserver.TCPServer((args.ip, args.port), MyHandler) as httpd:
-    httpd.socket = ssl_context.wrap_socket(httpd.socket, server_side=True)
-    print(f"[*] Serving directory at https://{args.ip}:{args.port} from {args.directory}")
-    httpd.serve_forever()
+def main():
+    args = parse_arguments()
+
+    if args.no_https:
+        if any([args.cert, args.key]):
+            print("[-] Cannot use Certificate and private key in HTTP")
+            exit(1)
+        else:
+            serve_http(args)
+    else:
+        serve_https(args)
+
+if __name__ == "__main__":
+    main()
