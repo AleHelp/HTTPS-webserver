660bf75b82a002c776129f2adb4c09f83b00a4da
Alessandro Eleuteri <126585573+AleHelp@users.noreply.github.com>
2024-01-08 18:23:04 +0100

    Update https_server.py
    Update source code, add listing network interfaces, fixing some errors

--- https_server.py
+++ https_server.py
@@ -6,20 +6,16 @@ import ssl
 import argparse
 import os
 import socket
+import psutil
+import sys
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='Simple web server over HTTPS.')
-    parser.add_argument('-i', '--ip', type=str, default="0.0.0.0", help='Specify an IP address (default: 0.0.0.0)')
-    parser.add_argument('-p', '--port', type=int, default=443, help='Select a port (default: 443)')
-    parser.add_argument('-d', '--directory', type=str, default="./", help='Select a directory (default is the current directory where you start it)')
-    parser.add_argument('-c', '--cert', type=str, help='Specify a certificate to use')
-    parser.add_argument('-k', '--key', type=str, help='Specify a private key')
-    parser.add_argument('--no-https', action='store_true', help='Serve over HTTP.')
-    parser.epilog = ('To generate a self-signed certificate and the corresponding private key, use the following command: openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365.')
-    return parser.parse_args()
+class MyHTTPServer(socketserver.TCPServer):
+    def server_bind(self):
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        super().server_bind()
 
 def serve_http(args):
-    print(f"[+] Serving directory at http://{args.ip}:{args.port} from {args.directory}")
+    print(f"[+] Serving over HTTP at http://{args.ip}:{args.port} from {args.directory}")
     os.chdir(args.directory)
     server_class = HTTPServer
     server_address = (args.ip, args.port)
@@ -37,26 +33,51 @@ def serve_https(args):
     ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
     ssl_context.load_cert_chain(certfile=args.cert, keyfile=args.key)
 
-    print(f"[+] Serving directory at https://{args.ip}:{args.port} from {args.directory}")
-    httpd = socketserver.TCPServer((args.ip, args.port), SimpleHTTPRequestHandler)
-    httpd.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
+    os.chdir(args.directory)
+    print(f"[+] Serving over HTTPS at https://{args.ip}:{args.port} from {args.directory}")
+    httpd = MyHTTPServer((args.ip, args.port), SimpleHTTPRequestHandler)
+    httpd.socket = ssl_context.wrap_socket(httpd.socket, server_side=True)
     with httpd:
-        httpd.socket = ssl_context.wrap_socket(httpd.socket, server_side=True)
         try:
             httpd.serve_forever()
         except KeyboardInterrupt:
             print("\n[!] Server stopped by user.")
 
+def list_interfaces():
+    interfaces = psutil.net_if_addrs()
+    print(f"[+] Listing network interfaces:\n")
+    for interface_name, addresses in interfaces.items():
+        print(f"Interface: {interface_name}")
+        for address in addresses:
+            if address.family == socket.AF_INET:
+                print(f"address: {address.address}\n")
+
 def main():
-    args = parse_arguments()
+    parser = argparse.ArgumentParser(description='Simple web server over HTTPS and also in HTTP.')
+    parser.add_argument('-l', '--list', action='store_true', help='List all network interfaces')
+    parser.add_argument('-i', '--ip', type=str, default="0.0.0.0", help='Specify an IP address (default: 0.0.0.0)')
+    parser.add_argument('-p', '--port', type=int, default=443, help='Select a port (default: 443 for HTTPS, 80 for HTTP)')
+    parser.add_argument('-d', '--directory', type=str, default="./", help='Select a directory (default is the current directory where you start it)')
+    parser.add_argument('-c', '--cert', type=str, help='Specify a certificate')
+    parser.add_argument('-k', '--key', type=str, help='Specify a private key')
+    parser.add_argument('--no-https', action='store_true', help='Serve over HTTP')
+    parser.epilog = ('To generate a self-signed certificate and the corresponding private key, use the following command: openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365.')
+    args = parser.parse_args()
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        exit(0)
+
+    if args.list:
+        list_interfaces()
 
     if args.no_https:
         if any([args.cert, args.key]):
             print("[-] Cannot use Certificate and private key in HTTP")
             exit(1)
-        else:
-            serve_http(args)
+        if args.port == 443:
+            args.port = 80
+        serve_http(args)
     else:
         serve_https(args)
 
